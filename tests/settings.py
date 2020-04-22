31d54e8c5e936a1a0c3831d1d14c96044a793ae3
dawnwages <dawn.wages@gmail.com>
2020-04-22 14:21:43 -0400

    II-210: URLS included in experiment

--- tests/settings.py
+++ tests/settings.py
@@ -42,6 +42,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'experiments.middleware.GoalURLMiddleware',
             ],
             'debug': True,
         },
