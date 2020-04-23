782983c4b2e4b68a75b3eba7ad8568e16f1b680d
dawnwages <dawn.wages@gmail.com>
2020-04-23 13:36:56 -0400

    #210: path and url

--- experiments/middleware.py
+++ experiments/middleware.py
@@ -9,7 +9,9 @@ from .utils import get_user_id
 
 class GoalURLMiddleware(MiddlewareMixin):
     def process_request(self, request):
+        current_full_url = request.get_full_path()
         current_url = request.path
+        print(current_url)
         # does the current URL matches the goal URL for a live experiment?
         experiments = Experiment.objects.filter(
             goal_url__contains=current_url,
@@ -20,5 +22,7 @@ class GoalURLMiddleware(MiddlewareMixin):
             user_id = get_user_id(request)
             for experiment in experiments:
                 experiment.record_completion_for_user(user_id, request)
+        else:
+            print('No {} experiments on url {}'.format(current_full_url, current_url))
         # If the current_url is not an experiment's goal_url, then don't do anything
         return None
