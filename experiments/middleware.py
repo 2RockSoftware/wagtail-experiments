c7b74b891482c3bff7915589c3793fe2717bfa3b
dawnwages <dawn.wages@gmail.com>
2020-04-29 12:56:32 -0400

    #210: clean up debugging

--- experiments/middleware.py
+++ experiments/middleware.py
@@ -9,9 +9,7 @@ from .utils import get_user_id
 
 class GoalURLMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        current_full_url = request.get_full_path()
         current_url = request.path
-        print(current_url)
         # does the current URL matches the goal URL for a live experiment?
         experiments = Experiment.objects.filter(
             goal_url__contains=current_url,
@@ -23,6 +21,5 @@ class GoalURLMiddleware(MiddlewareMixin):
             for experiment in experiments:
                 experiment.record_completion_for_user(user_id, request)
         else:
-            print('No {} experiments on url {}'.format(current_full_url, current_url))
         # If the current_url is not an experiment's goal_url, then don't do anything
         return None
